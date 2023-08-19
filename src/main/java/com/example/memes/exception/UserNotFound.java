c595b4ec106f07acbab1dd08ec2e4f70fe9ed9e9
Aleksei Markevich <markevi4.Alex@yandex.ru>
2023-08-19 19:07:58 +0300

    refactor: refactor exception class code

--- src/main/java/com/example/memes/exception/UserNotFound.java
+++ src/main/java/com/example/memes/exception/UserNotFound.java
@@ -1,5 +1,10 @@
 package com.example.memes.exception;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class UserNotFound extends RuntimeException {
 
     public UserNotFound(String message) {
